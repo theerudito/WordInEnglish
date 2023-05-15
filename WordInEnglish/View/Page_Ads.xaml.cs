15939d4d2408c026be13985ac53ce12b10aa86bf
theerudito <erudito.tv@gmail.com>
2023-05-15 11:33:50 -0500

    v5

---
+++ WordInEnglish/View/Page_Ads.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WordInEnglish.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_Ads : ContentView
+    {
+        public Page_Ads()
+        {
+            InitializeComponent();
+        }
+    }
+}
